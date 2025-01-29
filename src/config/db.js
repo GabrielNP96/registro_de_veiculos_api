e32436d514d9d92e8f764ac901c8a1894e1b8d7c
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-01-29 15:58:18 +0000

    add

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+
+const connectDb = async () => {
+    try {
+     
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('✅ Conectado ao MongoDB Atlas com sucesso!');
+        process.exit(1);
+} catch(error) {
+    console.error('❌ Erro ao conectar ao MongoDB:', error);
+    process.exit(1);
+}
+}
+
+export default connectDb;
---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express, { json } from 'express';
+import connectDB from './config/db';
+
+const app = express();
+
+// Conecta ao MongoDB
+connectDB();
+
+app.use(json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));

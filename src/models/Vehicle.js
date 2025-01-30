a7f0a2735a36ac61dc0fa32ec30c43271d34f536
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-01-30 15:49:45 +0000

    Schema criado com sucesso

---
+++ src/models/Vehicle.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const VehicleSchema = new mongoose.Schema({
+    model: {
+        type: String,
+        required: true
+    },
+    manufacturer: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    engine: {
+        type: Number,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    }
+})
+
+const car = mongoose.model("Car", VehicleSchema);
+const motorcycle = mongoose.model("Motorcycle", VehicleSchema);
+
+export  {car, motorcycle};

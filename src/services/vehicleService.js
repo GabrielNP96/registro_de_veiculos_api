2e27f389de0cc7e04130a02fa5ae5633baf06c33
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-01-31 13:24:52 +0000

    serviço criado

--- src/services/vehicleService.js
+++ src/services/vehicleService.js
@@ -1,7 +1,29 @@
-import {car, motorcycle} from "../models/Vehicle.js"
+import { car, motorcycle } from "../models/Vehicle.js";
 
+/**
+ * Salva um veículo na coleção correta (Car ou Motorcycle)
+ * @param {Object} vehicleData - Dados do veículo
+ * @param {string} vehicleData.type - Tipo do veículo (car ou motorcycle)
+ * @returns {Object} - Veículo salvo no banco
+ */
 export const saveVehicle = async (vehicleData) => {
     try {
+        if (!vehicleData.type) {
+            throw new Error("O campo 'type' é obrigatório ('car' ou 'motorcycle').");
+        }
 
+        let model;
+        if (vehicleData.type === "car") {
+            model = car;
+        } else if (vehicleData.type === "motorcycle") {
+            model = motorcycle;
+        } else {
+            throw new Error("Tipo de veículo inválido. Use 'car' ou 'motorcycle'.");
+        }
+
+        const newVehicle = new model(vehicleData);
+        return await newVehicle.save();
+    } catch (error) {
+        throw new Error(`Erro ao salvar veículo: ${error.message}`);
     }
-}
+};
